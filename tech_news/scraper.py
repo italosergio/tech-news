f18846c1121e447bbf634be74d000106c910238e
Italo Sergio <italosergio@graduate.org>
2022-09-21 19:15:31 -0300

    pass: requirement 1 - url scraper

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,22 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            url, timeout=3, headers={"User-Agent": "Fake user-agent"}
+        )
+        response.raise_for_status()
+        time.sleep(1)
+        return response.text
+
+    except requests.ReadTimeout:
+        return None
+
+    except requests.exceptions.HTTPError:
+        return None
 
 
 # Requisito 2
