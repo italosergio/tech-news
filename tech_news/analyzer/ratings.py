33df04148ac06eb4c8011603072871cbb11f2493
Italo Sergio <italosergio@graduate.org>
2022-09-21 23:30:45 -0300

    pass: requirement 10 - return news top 5

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = sorted(find_news(), key=lambda x: x["comments_count"], reverse=1)
+    sorted(news, key=lambda x: x["title"])
+
+    max_iterations = 5
+    news_list_size = len(news)
+
+    if news_list_size < max_iterations:
+        max_iterations = news_list_size
+
+    top_5 = list()
+    for index in range(0, max_iterations):
+        top_5.append((news[index]["title"], news[index]["url"]))
+
+    return top_5
 
 
 # Requisito 11
