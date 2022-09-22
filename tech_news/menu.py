0fdbe58cf273acb9131a3e112ce575d6a5c02cac
Italo Sergio <italosergio@graduate.org>
2022-09-22 06:05:42 -0300

    pass: requirement 13 - using functions

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,7 +1,28 @@
 import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_tag,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
 
 
 # Requisito 12
+def return_by_option(index, input_options, function_options):
+    if int(index) == 0:
+        return function_options[int(index)](int(index))
+    elif int(index) == 7:
+        print("Encerrando script")
+    else:
+        if input_options[int(index)] != "":
+            response = input(input_options[int(index)])
+            return function_options[int(index)](str(response))
+        else:
+            return function_options[int(index)]()
+
+
 def analyzer_menu():
     print(
         "Selecione uma das opções a seguir:\n "
@@ -14,19 +35,32 @@ def analyzer_menu():
         "6 - Listar top 5 categorias;\n "
         "7 - Sair."
     )
-    options = [
+    input_options = [
         "Digite quantas notícias serão buscadas:",
         "Digite o título:",
         "Digite a data no formato aaaa-mm-dd:",
         "Digite a tag:",
         "Digite a categoria:",
+        "",
+        "",
+        "",
+    ]
+    functions = [
+        get_tech_news,
+        search_by_title,
+        search_by_date,
+        search_by_tag,
+        search_by_category,
+        top_5_news,
+        top_5_categories,
     ]
     INPUT_INDEX = input("Enter number: ")
 
     try:
-        if 0 <= int(INPUT_INDEX) <= 5:
-            print(options[int(INPUT_INDEX)])
+        if 0 <= int(INPUT_INDEX) <= 7:
+            return return_by_option(INPUT_INDEX, input_options, functions)
         else:
             print("Opção inválida", file=sys.stderr)
+
     except ValueError:
         print("Digite um número válido", file=sys.stderr)
