c56f013e70ce5b571d41c7df6a8cd9e64bfa748f
Italo Sergio <italosergio@graduate.org>
2022-09-22 05:35:35 -0300

    pass: requirement 12 - using functions

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,32 @@
+import sys
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    print(
+        "Selecione uma das opções a seguir:\n "
+        "0 - Popular o banco com notícias;\n "
+        "1 - Buscar notícias por título;\n "
+        "2 - Buscar notícias por data;\n "
+        "3 - Buscar notícias por tag;\n "
+        "4 - Buscar notícias por categoria;\n "
+        "5 - Listar top 5 notícias;\n "
+        "6 - Listar top 5 categorias;\n "
+        "7 - Sair."
+    )
+    options = [
+        "Digite quantas notícias serão buscadas:",
+        "Digite o título:",
+        "Digite a data no formato aaaa-mm-dd:",
+        "Digite a tag:",
+        "Digite a categoria:",
+    ]
+    INPUT_INDEX = input("Enter number: ")
+
+    try:
+        if 0 <= int(INPUT_INDEX) <= 5:
+            print(options[int(INPUT_INDEX)])
+        else:
+            print("Opção inválida", file=sys.stderr)
+    except ValueError:
+        print("Digite um número válido", file=sys.stderr)
