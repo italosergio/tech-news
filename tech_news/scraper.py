3c54403b64fd97c2673d5e33f360088ffefb11c8
Italo Sergio <italosergio@graduate.org>
2022-09-21 21:28:19 -0300

    pass: requirement 4 - return info news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -35,16 +35,38 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     next_page_link = (
-        Selector(html_content)
-        .css("a.next.page-numbers::attr(href)")
-        .get()
+        Selector(html_content).css("a.next.page-numbers::attr(href)").get()
     )
     return next_page_link
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".title-author a::text").get().strip()
+    comments_count = len(selector.css(".comments").getall())
+    summary = (
+        selector.xpath("string(//div[@class='entry-content']/p)").get().strip()
+    )
+    tags = selector.css("a[rel='tag']::text").getall()
+    category = selector.xpath("string(//span[@class='label'])").get().strip()
+
+    info_news = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
+
+    return info_news
 
 
 # Requisito 5
